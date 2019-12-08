79f68b1ac90641f4565adf43e11e7bf862950e6a
maniek2332 <maniek2332@gmail.com>
2019-12-08 20:26:17 +0100

    build process updates for python3.8

--- scripts/docker_wheel_builder.sh
+++ scripts/docker_wheel_builder.sh
@@ -21,6 +21,10 @@ then
             PY_VERSION="python3.7m"
             PY_VERSION_ABI="cp37-cp37m"
             ;;
+        "py38")
+            PY_VERSION="python3.8"
+            PY_VERSION_ABI="cp38-cp38"
+            ;;
         *)
             echo "ERROR: Unknown py version specified: $1"
             exit 1
@@ -34,6 +38,7 @@ fi
 echo "Building for: ${PY_VERSION} (${PY_VERSION_ABI})"
 
 PATH="/opt/python/${PY_VERSION_ABI}/bin:$PATH"
+python --version
 
 yum install -y alsa-lib-devel pulseaudio-libs-devel  # SDL audio dependencies
 yum install -y libXrandr-devel libXcursor-devel  # SDL video dependencies
