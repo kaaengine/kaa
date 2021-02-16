33874e8f8a08be86e793c10706b06674a61ef330
maniek2332 <maniek2332@gmail.com>
2021-02-16 18:42:50 +0100

    added more options to stats_graph

--- src/kaa/tools/stats_graph.py
+++ src/kaa/tools/stats_graph.py
@@ -1,4 +1,5 @@
 import re
+import dataclasses
 import typing
 import queue
 import threading
@@ -73,14 +74,22 @@ class ServerRequestHandler(socketserver.DatagramRequestHandler):
             self.server.data_queue.put_nowait(parsed_stats)
 
 
+@dataclasses.dataclass
+class GraphSettings:
+    subplot_patterns: typing.List[re.Pattern] = \
+        dataclasses.field(default_factory=lambda: SUBPLOT_PATTERNS)
+    selected_stats: typing.List[re.Pattern] = \
+        dataclasses.field(default_factory=list)
+
+
 class GraphingServer:
     def __init__(self, server_host: str = DEFAULT_SERVER_HOST,
                  server_port: int = DEFAULT_SERVER_PORT,
-                 subplot_patterns=SUBPLOT_PATTERNS):
+                 graph_settings: typing.Optional[GraphSettings] = None):
         self.data_queue = queue.Queue()
         self.graph = Graph(
             data_queue=self.data_queue,
-            subplot_patterns=subplot_patterns,
+            settings=graph_settings or GraphSettings(),
         )
 
         self.udp_server = GraphingUDPServer(
@@ -97,12 +106,15 @@ class GraphingServer:
 
 class Graph:
     def __init__(self, data_queue: queue.Queue,
-                 subplot_patterns):
+                 settings: GraphSettings):
         self.data_queue = data_queue
-        self.subplot_patterns = subplot_patterns
+        self.settings = settings
 
-        needed_subplots_count = len(self.subplot_patterns) + 1
+        needed_subplots_count = len(self.settings.subplot_patterns) + 1
         self.figure, self.axes = plt.subplots(needed_subplots_count, 1)
+        # for single plot returned axes will not be an iterable
+        if not isinstance(self.axes, np.ndarray):
+            self.axes = np.ndarray([self.axes])
         plt.subplots_adjust(left=0.1, right=0.99, top=0.95, bottom=0.05)
         for ax in self.axes:
             ax.set_xlim(-PLOT_DISPLAYED_VALUES, 0)
@@ -119,13 +131,22 @@ class Graph:
         self.y_data_offset = PLOT_DISPLAYED_VALUES
 
         self.stat_name_metadata_map = {}
+        self.ignored_stats = set()
 
         self.default_ax, *other_axes = self.axes
         self.pattern_subplots_map = {
-            pattern: ax for pattern, ax in zip(self.subplot_patterns, other_axes)
+            pattern: ax for pattern, ax in zip(self.settings.subplot_patterns,
+                                               other_axes)
         }
 
     def _add_new_stat(self, stat_name):
+        if self.settings.selected_stats:
+            for selected_stat_pattern in self.settings.selected_stats:
+                if selected_stat_pattern.match(stat_name):
+                    break
+            else:
+                self.ignored_stats.add(stat_name)
+                return False
         next_index, _ = self.y_data.shape
         for pattern, ax in self.pattern_subplots_map.items():
             if pattern.search(stat_name):
@@ -139,11 +160,15 @@ class Graph:
         new_y_row = np.ndarray((1, PLOT_DISPLAYED_VALUES * 2))
         new_y_row.fill(float('nan'))
         self.y_data = np.vstack((self.y_data, new_y_row))
+        return True
 
     def _add_values(self, stats):
         for stat_name, value in stats:
             if stat_name not in self.stat_name_metadata_map:
-                self._add_new_stat(stat_name)
+                if stat_name in self.ignored_stats:
+                    continue
+                elif not self._add_new_stat(stat_name):
+                    continue
             y_index, _ = self.stat_name_metadata_map[stat_name]
             self.y_data[y_index, self.y_data_offset] = value
 
@@ -160,7 +185,6 @@ class Graph:
         except queue.Empty:
             pass
 
-        # TODO detect changes?
         changed_lines = []
         if self.needs_redraw:
             for y_index, line in self.stat_name_metadata_map.values():
@@ -199,11 +223,23 @@ if __name__ == '__main__':
                         help="Port to listen on (default: {})"
                         .format(DEFAULT_SERVER_PORT),
                         default=DEFAULT_SERVER_PORT)
+    parser.add_argument('-1', '--single-plot', action='store_true', dest='single_plot',
+                        help="Don't use subplots")
+    parser.add_argument('-s', '--select', action='append', dest='selected_stats',
+                        type=str,
+                        help="Select only specified stats, supports"
+                        " python regex syntax, can be used multiple times")
 
     args = parser.parse_args()
 
+    graph_settings = GraphSettings()
+    if args.single_plot:
+        graph_settings.subplot_patterns = []
+    if args.selected_stats:
+        graph_settings.selected_stats = [re.compile(s) for s in args.selected_stats]
+
     graphing_server = GraphingServer(
-        server_host=args.host, server_port=args.port,
+        server_host=args.host, server_port=args.port, graph_settings=graph_settings
     )
     print("Graph server is listening for UDP on: {}:{}"
           .format(args.host, args.port))
