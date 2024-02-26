a7a38f19653e406a0b1fd8568a41eb4d701eca5b
labuzm <labuzm@gmail.com>
2024-02-26 01:37:01 +0100

    upgrade manylinux2010_x86_64 to manylinux2014_x86_64
    manylinux2010_x86_64 doesn't support py310+

--- scripts/wheel_builder.sh
+++ scripts/wheel_builder.sh
@@ -4,13 +4,14 @@ set -x -e
 
 cd `dirname $0`/..
 
-DOCKER_IMAGE="quay.io/pypa/manylinux2010_x86_64"
+PLATFORM_TAG="manylinux2014_x86_64"
+DOCKER_IMAGE="quay.io/pypa/${PLATFORM_TAG}"
 
 if [ -n "$1" ]
 then
     if [ "$1" = "all" ]
     then
-        TARGETS="py35 py36 py37 py38 py39"
+        TARGETS="py36 py37 py38 py39 py310 py311 py312"
     else
         TARGETS="$1"
     fi
@@ -25,7 +26,9 @@ python -c 'import versioneer; versioneer.write_to_version_file("_build_version.p
 
 for PY_VERSION in ${TARGETS}
 do
-    sudo docker run --rm -it -v `pwd`:/host "${DOCKER_IMAGE}" \
+    sudo docker run --rm -it \
+        -e PLATFORM_TAG="${PLATFORM_TAG}" \
+        -v `pwd`:/host "${DOCKER_IMAGE}" \
         /bin/bash /host/scripts/docker_wheel_builder.sh ${PY_VERSION}
 done
 
