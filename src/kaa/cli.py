f9c2cc588767fb02361df1d1b09e06f6e565cc1b
labuzm <labuzm@gmail.com>
2024-03-11 00:55:03 +0100

    shader_tools.py revamped

--- src/kaa/cli.py
+++ src/kaa/cli.py
@@ -1,15 +1,16 @@
+import os
 import sys
 import logging
 import argparse
 from pathlib import Path
 
-from kaa.shader_tools import ShaderCompiler
+from kaa.shader_tools import CliShaderCompiler, UnsupportedPlatform
 
 logger = logging.getLogger(__name__)
 
 
 def shaderc():
-    compiler = ShaderCompiler()
+    compiler = CliShaderCompiler(raise_on_compilation_error=False)
     return compiler.compile(*sys.argv[1:])
 
 
@@ -17,10 +18,10 @@ def compile_shader():
     parser = argparse.ArgumentParser()
     parser.add_argument('source_file', help='path to shader source file')
     parser.add_argument(
-        'type', choices=ShaderCompiler.SUPPORTED_TYPES, help='shader type'
+        'type', choices=CliShaderCompiler.SUPPORTED_TYPES, help='shader type'
     )
     parser.add_argument(
-        '-p', '--platform', choices=ShaderCompiler.SUPPORTED_PLATFORMS,
+        '-p', '--platform', choices=CliShaderCompiler.SUPPORTED_PLATFORMS,
         nargs='+', required=True, help='target platform'
     )
     parser.add_argument(
@@ -28,18 +29,14 @@ def compile_shader():
         help='output directory, source file directory will be used, if not provided'
     )
     args = parser.parse_args()
-    compiler = ShaderCompiler(raise_on_compilation_error=False)
-
-    output_dir = Path(args.output_dir) if args.output_dir is not None else None
+    compiler = CliShaderCompiler(raise_on_compilation_error=False)
+    output_dir = args.output_dir or os.path.dirname(args.source_file)
     try:
-        result = compiler.compile_for_platforms(
-            platforms=args.platform, source_path=Path(args.source_file),
-            type_=args.type, output_dir=output_dir
+        compiler.compile_for_platform(
+            platform=args.platform, source_file=Path(args.source_file),
+            shader_type=args.type, output_dir=Path(output_dir)
         )
-    except RuntimeError as e:
+    except UnsupportedPlatform as e:
         logger.error(str(e))
         return 1
-
-    for shader_model, path in result.items():
-        logger.info('Compiled %s shader: %s', shader_model, path)
     return 0
